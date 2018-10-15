77596acd9b45b9c58835cc8548e433c127c74fa4
Robert Dodson <rad@robdodson.net>
2018-10-15 13:26:20 -0700

    Initial Commit

---
+++ Circuits/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Circuits
+//
+//  Created by Robert Dodson on 10/15/18.
+//  Copyright © 2018 Robert Dodson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
